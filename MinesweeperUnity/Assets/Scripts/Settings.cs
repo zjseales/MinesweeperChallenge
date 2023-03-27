a67dd0a46d27da431edfe80446b9b0ed3f191b00
DESKTOP-DAST5UK\Zac <zac_seales@live.com>
2023-03-27 19:19:33 +1300

    All options work, including volume slider. added a background audio loop.

--- MinesweeperUnity/Assets/Scripts/Settings.cs
+++ MinesweeperUnity/Assets/Scripts/Settings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Settings : MonoBehaviour
@@ -9,22 +10,28 @@ public class Settings : MonoBehaviour
     public GameObject optionsMenu;
     public GameObject difficultyMenu;
 
+    // The volume slider object
+    public Slider slider;
+
+    // bool defining whether fullscreen is active.
+    private bool fullscreenMode;
+
     /** Close all ui panels except for main menu
      *  and set initial fixed window size.
      */
     private void Awake()
     {
+        setVolume();
+        fullscreenMode = false;
         // show Main Menu
         openMain();
         //set window size
-        Screen.SetResolution(600, 800, true);
-        Screen.fullScreenMode = FullScreenMode.Windowed;
+        Screen.SetResolution(600, 800, fullscreenMode);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -43,19 +50,18 @@ public class Settings : MonoBehaviour
         switch (level)
         {
             case "LowRes":
-                Screen.SetResolution(640, 480, true);
+                Screen.SetResolution(640, 480, fullscreenMode);
                 break;
             case "MidRes":
-                Screen.SetResolution(1024, 800, true);
+                Screen.SetResolution(1024, 800, fullscreenMode);
                 break;
             case "HighRes":
-                Screen.SetResolution(1280, 720, true);
+                Screen.SetResolution(1280, 720, fullscreenMode);
                 break;
             case "EpicRes":
-                Screen.SetResolution(1920, 1080, true);
+                Screen.SetResolution(1920, 1080, fullscreenMode);
                 break;
         }
-        Screen.fullScreenMode = FullScreenMode.Windowed;
     }
 
     /** Set graphics quality dependent on the button pressed.
@@ -80,7 +86,22 @@ public class Settings : MonoBehaviour
                 QualitySettings.SetQualityLevel(3);
                 break;
         }
+    }
+
+    /** Toggle windowed screen.
+     */
+    public void windowed()
+    {
+        fullscreenMode = false;
         Screen.fullScreenMode = FullScreenMode.Windowed;
+    }
+
+    /** Toggle full screen.
+     */
+    public void fullScreen()
+    {
+        fullscreenMode = true;
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
     }
 
     /** Close all UI Panels and open Main menu.
@@ -106,6 +127,20 @@ public class Settings : MonoBehaviour
     {
         mainMenu.SetActive(false);
         difficultyMenu.SetActive(true);
+    }
+
+    /** Sets a default start volume and then adds a listener to the 
+     *  slider so that volume is changed as slider value changes.
+     */
+    public void setVolume()
+    {
+        //set initial volume
+        AudioListener.volume = 0.75f;
+        // set volume slider listener
+        slider.onValueChanged.AddListener((v) =>
+        {
+            AudioListener.volume = v;
+        });
     }
 
     /** Exit the Game and close window.
